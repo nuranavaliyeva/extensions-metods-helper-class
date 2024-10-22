d0fab6d47014342d47a83fb6676fc1c2902d6a42
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-10-22 21:11:37 +0400

    first commit

---
+++ Static class, Extensions methods,  Exception, Reflection/Helper.cs
@@ -0,0 +1,75 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Static_class__Extensions_methods___Exception__Reflection
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num > 0 && num % 2 == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if (num > 0 && num % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool HasDigit(this string value)
+        {
+            foreach(char c in value)
+            {
+                if(char.IsDigit(c))
+                    return true;
+            }
+            return false;
+
+
+        }
+       public static bool CheckPassword(this string value)
+        {
+            if (value.Length < 8)
+                return false;
+
+            bool hasDigit = false;
+            bool hasUpper = false;
+
+            foreach (char c in value)
+            {
+                if (char.IsDigit(c))
+                {
+                    hasDigit = true;
+                }
+
+                if (char.IsUpper(c))
+                {
+                    hasUpper = true;
+                }
+
+                if (hasDigit && hasUpper)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static  string Capitalize(this string value)
+        {
+            return value.Substring(0, 1).ToUpper() + value.Substring(1).ToLower();
+        }
+
+
+    }
+}
+
---
+++ Static class, Extensions methods,  Exception, Reflection/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Static_class__Extensions_methods___Exception__Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 4;
+            Console.WriteLine(num.IsEven());
+            int num2 = 6;
+            Console.WriteLine(num.IsOdd());
+            string word = "1nurana";
+            Console.WriteLine(word.HasDigit());
+            string Password = "nuraNa123";
+            Console.WriteLine(Password.CheckPassword());
+            string word2 = "nurana";
+            Console.WriteLine(word2.Capitalize());
+            
+        }
+    }
+}
